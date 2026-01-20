3c1b5ae937f17db885735f7db57570105c64edb7
psigfry <darn.vr@gmail.com>
2026-01-20 20:20:49 +0300

    Сделал базовую структуру

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -1,1 +1,25 @@
 <?php
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+use App\Models\QuizResult;
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    // список квизов
+    public function index()
+    {
+        $quizzes = Quiz::all();
+
+        return view('quizzes.index', compact('quizzes'));
+    }
+
+    // страница квиза
+    public function show($id)
+    {
+        $quiz = Quiz::findOrFail($id);
+
+        return view('quizzes.show', compact('quiz'));
+    }
+}
