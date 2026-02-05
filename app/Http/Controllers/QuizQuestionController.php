9b5553b3e61d74ef957d36b3b80bda3b528596fd
AlexeyKilnik <kilnikaa@yandex.ru>
2026-02-05 21:17:20 +0300

    Сделали метод show и answer

--- app/Http/Controllers/QuizQuestionController.php
+++ app/Http/Controllers/QuizQuestionController.php
@@ -1,1 +1,40 @@
 <?php
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+
+class QuizQuestionController extends Controller
+{
+    public function show($id, $number)
+    {
+        $quiz = Quiz::find($id);
+        $question = $quiz->questions()->find($number);
+
+        if ($number === 1){
+            session()->forget("quiz_{$quiz->id}.results");
+        }
+
+        if (!$question) {
+            return redirect()->route('quizzes.finish', $quiz->id);
+        }
+
+        return view('quizzes.question', [
+            'quiz' => $quiz,
+            'question' => $question,
+            'number' => $number
+        ]);
+    }
+
+    public function answer($id, $number){
+        $quiz = Quiz::find($id);
+
+        $isCorrect = request('answer') === '1';
+
+        session()->push("quiz_{$quiz->id}.results", $isCorrect);
+
+        return redirect()->route('quizzes.question', [
+            'id' => $quiz->id,
+            'number' => $number + 1
+        ]);
+    }
+}
