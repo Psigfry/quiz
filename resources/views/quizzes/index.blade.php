8e98a455441e76f7628f912b2f10f3af373f9dd2
AlexeyKilnik <kilnikaa@yandex.ru>
2026-03-24 20:44:04 +0300

    закоментировали кнопку "добавить квиз"

--- resources/views/quizzes/index.blade.php
+++ resources/views/quizzes/index.blade.php
@@ -15,10 +15,10 @@
                     </div>
 
                     <div>
-                        <a href="#"
-                           class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
-                            Добавить квиз
-                        </a>
+{{--                        <a href="#"--}}
+{{--                           class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">--}}
+{{--                            Добавить квиз--}}
+{{--                        </a>--}}
                     </div>
                 </div>
 
