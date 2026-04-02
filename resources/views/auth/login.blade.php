ca11458e33c082cacd902ae055f2f7be4f0614f6
AlexeyKilnik <kilnikaa@yandex.ru>
2026-04-02 19:11:48 +0300

    перевели текст на русский и добавили пробелы между кнопками

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,22 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+            <x-nav-link :href="route('register')"  class="ms-4">
+                Регистрация
+            </x-nav-link>
+
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Забыли пароль?') }}
                 </a>
             @endif
 
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('Войти') }}
             </x-primary-button>
+
+
         </div>
     </form>
 </x-guest-layout>
